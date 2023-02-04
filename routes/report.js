19d149490754f45ec6e58ed8be10b16fc4f679b8
Eran <erand1998@gmail.com>
2023-02-04 17:43:20 +0200

    renamed variables

--- routes/report.js
+++ routes/report.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const cost = require('../models/costSchema');
+const costSchema = require('../models/costSchema');
 
 
 router.get('/report/:year/:month/:id', function (req, res, next) {
-    cost.findOne({user_id: req.params.id, year: req.params.year, month: req.params.month}, {
+    costSchema.findOne({user_id: req.params.id, year: req.params.year, month: req.params.month}, {
         category: 1
     }).then((cost) => {
         const {category, ...rest} = cost;
